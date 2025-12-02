f3386047ce4b030102187dc59388b942c13ef5df
Harish Bonu <23341a1215@gmrit.edu.in>
2025-12-03 00:11:15 +0530

    Add Tailwind CSS configuration file

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#00d4ff',
+        dark: '#0a0e27',
+        darkSecondary: '#1a1f3a',
+      },
+    },
+  },
+  plugins: [],
+}
